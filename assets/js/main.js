4ab385a9ba2e08082c28bb7b2fd984c00f04ab21
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2023-01-09 12:25:28 +0530

    changes + new pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,6 +68,60 @@
     }
     slideTopEffect('.header-section');
 
+    // Show Tabs 
+    const showTabs = (tabLinkID, tabContentID) => {
+        let tabLinks = document.querySelectorAll(tabLinkID),
+            tabContent = document.querySelectorAll(tabContentID);
+
+        if (tabLinks && tabContent) {
+            const openTabs = el => {
+                let selectedLink = el.currentTarget.classList,
+                    showId = el.currentTarget.dataset.tab;
+
+                tabLinks.forEach(el => {
+                    el.classList.remove("active");
+                });
+                tabContent.forEach(el => {
+                    el.classList.remove("active");
+                });
+                selectedLink.add("active");
+                document.querySelector("#" + showId).classList.add("active");
+            }
+            tabLinks.forEach(el => {
+                el.addEventListener("click", openTabs);
+            });
+        }
+    }
+    showTabs('[data-tab]', '.tab-content');
+
+    // Client Swiper
+    const clientSwiper = document.querySelector("#clientSwiper");
+    if (clientSwiper && window.innerWidth <= 1024) {
+        const swiper = new Swiper('#clientSwiper', {
+            // Optional parameters
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#clientSwiper .swiper-pagination",
+            },
+        });
+    }
+
+    // Work Swiper
+    const workSwiper = document.querySelector("#workSwiper");
+    if (workSwiper && window.innerWidth <= 768) {
+        const swiper = new Swiper('#workSwiper', {
+            // Optional parameters
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#workSwiper .swiper-pagination",
+            },
+        });
+    }
+
     // Blog Swiper
     const blogSwiper = document.querySelector("#blogSwiper");
     if (blogSwiper) {
@@ -77,6 +131,9 @@
             spaceBetween: 20,
             loop: true,
             grabCursor: true,
+            pagination: {
+                el: "#blogSwiper .swiper-pagination",
+            },
             breakpoints: {
                 640: {
                     slidesPerView: 2,
@@ -84,6 +141,62 @@
                 1024: {
                     slidesPerView: 3,
                 },
+            },
+        });
+    }
+
+    // All Work Swiper
+    const allWorkSwiper = document.querySelector("#allWorkSwiper");
+    if (allWorkSwiper && window.innerWidth <= 768) {
+        const swiper = new Swiper('#allWorkSwiper', {
+            // Optional parameters
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#allWorkSwiper .swiper-pagination",
+            },
+        });
+    }
+
+    // E-commerce Swiper
+    const eCommerceSwiper = document.querySelector("#eCommerceSwiper");
+    if (eCommerceSwiper && window.innerWidth <= 768) {
+        const swiper = new Swiper('#eCommerceSwiper', {
+            // Optional parameters
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#eCommerceSwiper .swiper-pagination",
+            },
+        });
+    }
+
+    // Branding Swiper
+    const brandingSwiper = document.querySelector("#brandingSwiper");
+    if (brandingSwiper && window.innerWidth <= 768) {
+        const swiper = new Swiper('#brandingSwiper', {
+            // Optional parameters
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#brandingSwiper .swiper-pagination",
+            },
+        });
+    }
+
+    // Mobile Application Swiper
+    const mobileApplicationSwiper = document.querySelector("#mobileApplicationSwiper");
+    if (brandingSwiper && window.innerWidth <= 768) {
+        const swiper = new Swiper('#mobileApplicationSwiper', {
+            // Optional parameters
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            pagination: {
+                el: "#mobileApplicationSwiper .swiper-pagination",
             },
         });
     }
